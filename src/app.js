688e546b2d2bc876a989cea697aeffdd1cc39285
M3 <34512995+Grublux@users.noreply.github.com>
2024-02-14 21:46:49 +0000

    Grublux Domain Gen 1st commit

--- src/app.js
+++ src/app.js
@@ -7,5 +7,21 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let pronoun = ["a", "our", "my"];
+  let adj = ["fancy", "big", "descriptive"];
+  let noun = ["story", "jungle", "crackhouse"];
+  let domain = [".com", ".io", ".net"];
+  let finalDomain = [];
+
+  for (let x of pronoun) {
+    for (let a of adj) {
+      for (let n of noun) {
+        for (let d of domain) {
+          var temp = x + a + n + d;
+          document.getElementById("domains").innerHTML += `<li>${temp}</li>`;
+        }
+      }
+    }
+  }
+  // document.getElementById("domains").innerHTML = "<p>Hello</p>";
 };
